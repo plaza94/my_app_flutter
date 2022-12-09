937673b95797412aa4e32eb149db95100403a571
Claudio Plaza <plza0000@gmail.com>
2022-12-09 16:40:03 -0300

    buildcontext Devtools y Scafold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('hola desde homeScreen'),
+    return const Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Text('hola desde homeScreen'),
+      ),
     );
   }
 }
