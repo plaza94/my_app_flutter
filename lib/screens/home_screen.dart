1f19275f94018bb8e50b337fc84c5ea0f855e689
Claudio Plaza <plza0000@gmail.com>
2022-12-09 17:35:53 -0300

    Widget basicos Columna - centrar widge

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,10 +5,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 10.0,
+      ),
       backgroundColor: Colors.amber,
       body: Center(
-        child: Text('hola desde homeScreen'),
+        child: Column(
+          //crossAxisAlignment:
+          // CrossAxisAlignment.end, //centra desde izquierda a derecha
+          mainAxisAlignment:
+              MainAxisAlignment.center, //centra desde arriba hacia abajo
+          children: const <Widget>[Text('Contador de Clicks'), Text('0.0')],
+        ),
       ),
     );
   }
