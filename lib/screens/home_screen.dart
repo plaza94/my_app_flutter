2a49b334cbf4739befea478c22848baf05e996e4
Claudio Plaza <plza0000@gmail.com>
2022-12-09 17:36:20 -0300

    estilos de texto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const TextStyle fontSize30 = const TextStyle(fontSize: 30);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeScreen'),
@@ -17,7 +19,16 @@ class HomeScreen extends StatelessWidget {
           // CrossAxisAlignment.end, //centra desde izquierda a derecha
           mainAxisAlignment:
               MainAxisAlignment.center, //centra desde arriba hacia abajo
-          children: const <Widget>[Text('Contador de Clicks'), Text('0.0')],
+          children: const <Widget>[
+            Text(
+              'Contador de Clicks',
+              style: fontSize30,
+            ),
+            Text(
+              '0.0',
+              style: fontSize30,
+            )
+          ],
         ),
       ),
     );
