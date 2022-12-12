5181a6e60533ddccad6f8e9080a2c1b2c24aeaa2
Claudio Plaza <plza0000@gmail.com>
2022-12-12 11:53:34 -0300

    refactorizando codigo

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -11,6 +11,21 @@ class _CounterScreenState extends State<CounterScreen> {
   //variable , propiedad
   int counter = 0;
 
+  void increased() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void restart() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
@@ -42,41 +57,68 @@ class _CounterScreenState extends State<CounterScreen> {
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.plus_one),
-            onPressed: () {
-              //print('hola mundo');
-              counter++;
-              setState(() {});
-            },
-          ),
-          /*  const SizedBox(
-            width: 20,
-          ),*/
-          FloatingActionButton(
-            child: const Icon(Icons.restart_alt_sharp),
-            onPressed: () {
-              //print('hola mundo');
-              counter = 0;
-              setState(() {});
-            },
-          ),
-          /*const SizedBox(
-            width: 20,
-          ),*/
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_minus_1),
-            onPressed: () {
-              //print('hola mundo');
-              counter--;
-              setState(() {});
-            },
-          ),
-        ],
+      floatingActionButton: CustomFloatingActions(
+        increaseFn: increased,
+        decreaseFn: decrease,
+        restartFn: restart,
       ),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function restartFn;
+
+  const CustomFloatingActions({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.restartFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.plus_one),
+          onPressed: () => increaseFn(),
+          //() => setState(() => counter++)
+        ),
+        //print('hola mundo');
+        // counter++;
+        //setState(() {});
+
+        /*  const SizedBox(
+          width: 20,
+        ),*/
+        FloatingActionButton(
+          child: const Icon(Icons.restart_alt_sharp),
+          onPressed: () => restartFn(),
+          //() => setState(() => counter=0),
+          /*onPressed: () {
+            //print('hola mundo');
+            counter = 0;
+            setState(() {});
+          },*/
+        ),
+        /*const SizedBox(
+          width: 20,
+        ),*/
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1),
+          onPressed: () => decreaseFn(),
+          //() => setState(() => counter--),
+          /*onPressed: () {
+            //print('hola mundo');
+            counter--;
+            setState(() {});
+          },*/
+        ),
+      ],
     );
   }
 }
